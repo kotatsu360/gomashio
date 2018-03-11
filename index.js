885bf06ab3fa0d21c2919578bf7940ee1c478c19
Tatsuro MITSUNO <c.arabica.l@gmail.com>
2018-03-12 01:07:32 +0900

    add notification controll

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 const config = (function () {
   let config = require('./config/config.json');
-  const topLevelKeyList = ['account_map', 'ignore_event_map'];
+  const topLevelKeyList = ['account_map', 'ignore_event_map', 'repository_map'];
   for (let i = 0, len = topLevelKeyList.length; i < len; i++) {
     config[topLevelKeyList[i]] = config[topLevelKeyList[i]] || {};
   }
@@ -14,11 +14,33 @@ const link = function (url, text) {
   return '<' + url + '|' + text + '>';
 };
 
+// github 2 slack
 const g2s = function (user) {
   return config.account_map[user] || user;
 };
 
-const isIgnore = function (event, action) {
+// repository to channel
+const r2c = function(repository) {
+
+  if (Object.keys(config.repository_map).length === 0) {
+    return '';
+  }
+
+  const name = repository['name'];
+  let channel = null;
+
+  for ( let rule in config.repository_map ) {
+    let re = new RegExp(rule, 'i');
+    if (re.test(name)) {
+      channel = config.repository_map[rule];
+      break;
+    }
+  }
+
+  return channel;
+};
+
+const isIgnoreEvent = function (event, action) {
   const index = (config.ignore_event_map[event] || []).indexOf(action);
   return index !== -1;
 };
@@ -49,17 +71,29 @@ exports.handler = (event, context, callback) => {
   const payloadText = decodeURIComponent(event.body.replace(/^payload=/,''));
   const payload = JSON.parse(payloadText);
   const action = payload.action || '';
+  const repository = payload.repository || {};
 
   // [NOTE] CloudWatch Logs will display cleanly for text format than json format.
   console.info(githubEvent);
   console.info(payloadText);
   console.info(action);
+  console.info(repository);
 
-  if (isIgnore(githubEvent, action)) {
+  if (isIgnoreEvent(githubEvent, action)) {
     console.info('ignore event. nothing to do.');
     context.succeed(responce);
   }
 
+  if (Object.keys(repository).length === 0) {
+    console.info('repository is empty. nothing to do.');
+    context.succeed(responce);
+  }
+
+  const channel = r2c(repository);
+  if (channel === null) {
+    console.info('repository is not eligible for notification. nothing to do.');
+    context.succeed(responce);
+  }
   let text='';
   switch (githubEvent){
     case 'issue_comment':
@@ -99,13 +133,19 @@ exports.handler = (event, context, callback) => {
     context.succeed(responce);
   }
 
+  let params = {text: text, link_names: 1};
+  if (channel !== '') {
+    params['channel'] = channel;
+  }
+
   request({
     url: slack,
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    json: {text: text, link_names: 1}
+    json: params
   }, function () {
     console.info('post to slack.');
     context.succeed(responce);
   });
 };
+
